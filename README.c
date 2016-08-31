cc89141ba613708298b567a4b9bdba940289396b
dominicwency <wencyxavier@gmail.com>
2016-08-31 15:45:34 +0530

    Create README.c

---
+++ README.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+  printf("positive");
+  elseif(n<0)
+  {
+  printf("negative");
+  }
+  elseif(n==0)
+  {
+  printf"zero");
+  }
+  return 0;
+  }
